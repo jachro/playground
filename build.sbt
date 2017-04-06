a5bb3a8221a961299d9d32235544028ee3086188
Jakub Chrobasik <jakub.chrobasik@digital.voa.gov.uk>
2017-04-06 12:17:46 +0100

    Playing with cats' semigroups

--- build.sbt
+++ build.sbt
@@ -4,4 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.12.1"
 
+libraryDependencies += "org.typelevel" %% "cats" % "0.9.0"
+
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.1" % "test"
---
+++ src/test/scala/catslearning/SemigroupSpec.scala
@@ -0,0 +1,32 @@
+package catslearning
+
+import org.scalatest.{Matchers, WordSpec}
+import cats.Semigroup
+import cats.implicits._
+
+class SemigroupSpec extends WordSpec with Matchers {
+
+  "semigroup" should {
+
+    "work for scala lib types" in {
+      Semigroup[Int] combine(1, 2) shouldBe 3
+      Semigroup[Option[Int]] combine(Some(1), None) shouldBe Some(1)
+      Semigroup[Option[Int]] combine(Some(1), Some(2)) shouldBe Some(3)
+      Semigroup[List[Int]] combine(List(1, 2), List(3, 4)) shouldBe List(1, 2, 3, 4)
+      Semigroup[Set[Int]] combine(Set(1, 2), Set(2, 4)) shouldBe Set(1, 2, 4)
+      Semigroup[Int => Int].combine(_ + 1, _ + 2)(5) shouldBe 13
+    }
+
+    "work with inline syntax" in {
+      val option1: Option[Int] = Some(1)
+      val option2: Option[Int] = None
+      val option3: Option[Int] = Some(3)
+      option1 |+| option2 |+| option3 shouldBe Some(4)
+    }
+
+    "allow semigroups composition" in {
+      Map("foo" -> Map("bar" -> 5)) combine Map("foo" -> Map("bar" -> 6), "baz" -> Map()) shouldBe Map("foo" -> Map("bar" -> 11), "baz" -> Map())
+      Map("foo" -> Map("bar" -> 5)) |+| Map("foo" -> Map("bar" -> 6), "baz" -> Map()) shouldBe Map("foo" -> Map("bar" -> 11), "baz" -> Map())
+    }
+  }
+}
